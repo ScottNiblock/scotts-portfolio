51c5c860b4eec94b6c24d7ee913cd8721c234863
ScottNiblock <scottniblock16@gmail.com>
2024-07-02 17:06:12 -0500

    Added other componenents

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -3,12 +3,14 @@ import './projects.css'
 import gpt3 from '../../assets/gpt3.png'
 import construction from '../../assets/construction.jpeg'
 import {Project} from '../../components'
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
   return (
     <div className='port__projects section__padding' id='projects'>
       <div className='port__projects-heading'>
         <h1 className='gradient__text'>My Projects</h1>
+        <a href='https://github.com/ScottNiblock?tab=repositories'> <button type='button'> Check out my GitHub <FaGithub/></button></a>
     </div>
     <div className='port__projects-container'>
       <Project imgUrl={gpt3} title="GPT-3 Website" description="A website based on OpenAI's GPT-3 that demonstrates aesthetic and responsive web design. Content is intentionally redundant with some omitted functionality" webUrl='https://gpt3sn.scottniblock.com/' />
